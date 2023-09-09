04d8a33a9536977e2efc5c73c6e89d8c650e0602
MishB1 <88148320+MishB1@users.noreply.github.com>
2023-09-09 01:47:23 +0000

    practice

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,38 @@
 
 import 'package:flutter/material.dart';
 
-void main(List<String> args) {
-  runApp(const MyApp());
+void main() {
+  runApp( MyApp());
 }
 
+
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+   MyApp({super.key});
+
+
+  void greet() {
+  print('Hello Hamish');
+}
+
+void greetPerson(String name ) {
+  print('Hello ' + name);
+}
+
+Map user = {
+  'name': "Hamish Blay",
+  'age': 22,
+  'height': 1.8,
+  'school': "KNUST",
+  'church': 'FOLC',
+
+};
 
   @override
   Widget build(BuildContext context) {
+    
+    print(user['church']);
+    greetPerson("Blay");
+    greet();
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(),
